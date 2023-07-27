58ecff58181e35841465b980e69452b95171641a
stevier biko <stevierbiko@gmail.com>
2023-07-27 10:41:00 +0300

    added script file

---
+++ typing text/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".auto-type", {
+    strings: [
+      "product manager",
+      "product marketer",
+      "Product Designer",
+    ],
+
+    typeSpeed:100,
+    backSpeed:100,
+    loop: true,
+  });
